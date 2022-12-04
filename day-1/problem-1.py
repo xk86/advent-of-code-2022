cfea9091840482752fcf8ad4dbd97d01fed157c3
Kate Dixon <xk86d64@gmail.com>
2022-12-04 14:39:39 -0500

    problem 1 solved

--- day-1/problem-1.py
+++ day-1/problem-1.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3
+
+with open("input", "r") as f:
+    elf_cals = [0]
+    current_elf = 0
+    for line in f:
+        try:
+            cals = int(line)
+            elf_cals[current_elf] += cals
+        except ValueError:
+            current_elf += 1
+            elf_cals.append(0)
+
+    print(elf_cals)
